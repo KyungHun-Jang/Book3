9beed351054aa7ce279b5110e9cce40618f6f1ce
Byeonghun Hwang <byeonghwang.com>
2019-05-14 15:12:53 +0900

    Initial Commit

---
+++ Book3Tests/Book3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Book3Tests.swift
+//  Book3Tests
+//
+//  Created by ktds 10 on 14/05/2019.
+//  Copyright © 2019 kt ds. All rights reserved.
+//
+
+import XCTest
+@testable import Book3
+
+class Book3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
