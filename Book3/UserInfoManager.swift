b20b3c5c7c495fffeba9a68c2dbf3feef46d6f24
Byeonghun Hwang <byeonghwang.com>
2019-05-15 16:30:57 +0900

    finish

--- Book3/UserInfoManager.swift
+++ Book3/UserInfoManager.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+import Alamofire
+
 struct UserInfoKey {
     static let id = "LOGINID"
     static let username = "USERNAME"
@@ -59,4 +61,58 @@ class UserInfoManager {
         }
     }
     
+    var isLogin:Bool {
+        if self.id == nil || self.username == nil {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    
+    //Login
+    func login(username:String, password:String, success:(()->Void)? = nil, fail:((String)->Void)? = nil){
+        
+        //1.URL과 전송할 값 준비
+        let url = "http://localhost:3000/auth/login"
+        let param: Parameters = [
+            "username":username,
+            "password":password
+        ]
+        
+        //2.서버에 요청
+        let call = AF.request(url, method: .post, parameters: param, encoding: JSONEncoding.default)
+        
+        call.responseJSON{
+            res in
+            switch res.result {
+            case .success(let data):
+                if let response = data as? NSDictionary {
+                    if let successResult = response["success"] as? Bool,
+                        successResult == true {
+                        if let data = response["data"] as? NSDictionary {
+                            self.id = data["_id"] as? String
+                            self.username = data["username"] as? String
+                            self.name = data["name"] as? String
+                            self.token = data["token"] as? String
+                            
+                            success?()
+                        }
+                    }
+                }
+            case .failure(let error):
+                    fail?("오류발생: \(error)")
+            }
+        }
+    }
+    
+    func logout() -> Bool {
+        let ud = UserDefaults.standard
+        ud.removeObject(forKey: UserInfoKey.id)
+        ud.removeObject(forKey: UserInfoKey.name)
+        ud.removeObject(forKey: UserInfoKey.username)
+        ud.removeObject(forKey: UserInfoKey.token)
+        ud.synchronize()
+        return true
+    }
 }
